cddca2b94b1b3c37a00964b553cde53793d88d42
Maneesh Tewani <maneesh.tewani@gmail.com>
2014-12-07 16:31:38 -0600

    Added code in javascript file

--- working/app/fillEx/fill.js
+++ working/app/fillEx/fill.js
@@ -1,6 +1,12 @@
 angular.module('app.fillColor', [])
     .controller('fillCtrl', function($scope){
     	$scope.view = "instructions";
+    	$scope.htmlObj = {};
+    	$scope.htmlObj.html = "";
+    	$scope.$watch('htmlObj.html', function() {
+            $("#container").empty();
+            $("#container").append($scope.htmlObj.html);
+        })
      	$scope.pickBG = function(){
 		var option=Math.floor((Math.random()*3)+1)
 			if(option==1){
@@ -25,4 +31,28 @@ angular.module('app.fillColor', [])
 				return "yellow";
 			}
 		}
+		$scope.compile = function() {
+            if($scope.htmlObj.html && validate()){
+                alert("You SHALL PASS!");
+                gameData.inMiddleOfMove = false;
+                $location.url("/gameBoard");
+            }
+            else
+                $scope.error = true;
+        }
+         $scope.askForHint = function() {
+            $scope.hint = true;
+        }
+        function validate() {
+            var rows = $('table').find("tr");
+            var numOfRows = rows.length;
+            if($scope.rowsNeeded !== numOfRows)
+                return false
+            for(var i = 0; i < numOfRows; i++) {
+                var numOfColumns = rows.eq(i).find('td').length;
+                if(numOfColumns !== $scope.columnsNeeded)
+                    return false;
+            }
+            return true;
+        }
     })
