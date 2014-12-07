e73f4309f25416e0c8458acfa6ee985719f84d6c
Maneesh Tewani <maneesh.tewani@gmail.com>
2014-12-06 20:17:15 -0600

    Finished font-size exercise

--- working/app/fontsize/fontExercise.js
+++ working/app/fontsize/fontExercise.js
@@ -1,15 +1,20 @@
 angular.module('app.fontExercise', [])
-    .controller('sizeCtrl',  function($scope){
+    .controller('sizeCtrl',  function($scope, $location){
         $scope.$watch('html', function() {
 			$("#container").empty();
 			$("#container").append($scope.html);
 		})
         $scope.compile = function() {
-			if($scope.html){
-				alert("Free Pass! You SHALL PASS!");
-				$location.url("/exercise");
+        	console.log("Clicked!");
+			if($scope.html && $scope.html.indexOf("div") > -1 && $scope.html.indexOf("font-size") > -1){
+				alert("You SHALL PASS!");
+				$location.url("/exercise2");
 			}
 			else
 				$scope.error = true;
+		}
+
+		function validate() {
+			//HTML validate
 		}
     })
