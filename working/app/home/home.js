ee2b763e73eebf372f06491a9d5db169537dc81e
Maneesh Tewani <maneesh.tewani@gmail.com>
2014-12-07 22:57:33 -0600

    localStorage is working
    Added back animation.

--- working/app/home/home.js
+++ working/app/home/home.js
@@ -1,6 +1,13 @@
 angular.module('app.landing', [])
-	.controller('landingCtrl', function($location, gameData){
+	.controller('landingCtrl', function($location, $http){
 		this.playGame = function() {
 			$location.url('/gameBoard')
+		}
+		if(typeof(Storage) != "undefined") {
+			localStorage.setItem('points', 0);
+			var asdf = {};
+			localStorage.setItem('tiles', asdf);
+			localStorage.setItem('currentSpace', 0);
+			localStorage.setItem('inMiddleOfMove', false);
 		}
 	})
